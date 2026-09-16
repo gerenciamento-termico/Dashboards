d50ac1cd0fe0bf84bd54074a53ac57276e4991e1
Aura Auto Update <aura-auto-update@example.local>
2026-09-16 06:38:33 -0300

    Atualiza dashboards Aura - 2026-09-16 06:38:32

--- GESTAO_DISPOSITIVOS_STAGE_DATA.js
+++ GESTAO_DISPOSITIVOS_STAGE_DATA.js
@@ -1,6 +1,6 @@
 window.GESTAO_DISPOSITIVOS_STAGE_DATA = {
   "fonte": "VTC STAGE (vtc_stage.documentos)",
-  "geradoEm": "16/09/2026 06:25:49",
+  "geradoEm": "16/09/2026 06:36:16",
   "summary": {
     "ARES": {
       "totalEstoque": null,
@@ -38,7 +38,7 @@ window.GESTAO_DISPOSITIVOS_STAGE_DATA = {
       "semStatus": 0,
       "semData": 0,
       "estoqueSemQuantidade": 0,
-      "ultimaAtualizacao": "16/09/2026 06:19:20",
+      "ultimaAtualizacao": "16/09/2026 06:34:20",
       "totalRegistros": 109103
     },
     "SHIELD": {
@@ -77,7 +77,7 @@ window.GESTAO_DISPOSITIVOS_STAGE_DATA = {
       "semStatus": 0,
       "semData": 0,
       "estoqueSemQuantidade": 0,
-      "ultimaAtualizacao": "16/09/2026 06:19:20",
+      "ultimaAtualizacao": "16/09/2026 06:34:20",
       "totalRegistros": 116040
     }
   },
